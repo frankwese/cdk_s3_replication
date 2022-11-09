295139f2ebfa5c2ce173abb44851b93ffb9e0f92
frank.wesemann <frank.wesemann@kreuzwerker.de>
2022-11-09 15:48:10 +0100

    ReplicatedBucket is now a construct

--- aws/index.ts
+++ aws/index.ts
@@ -1,30 +1,62 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import {Duration} from 'aws-cdk-lib';
 
-import { ArchiveStack } from './stacks/archive'
+import {ReplicatedBucket} from './stacks/replicated-bucket';
+import {Key} from 'aws-cdk-lib/aws-kms';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import {BucketAccessControl, BucketEncryption} from 'aws-cdk-lib/aws-s3';
+import {Effect, PolicyStatement, ServicePrincipal} from 'aws-cdk-lib/aws-iam';
 
-const bucketName = 'wese'
-const option = {
-  bucketName,
-  env: {
-    region: 'eu-central-1'
-  },
-  replications: [
-    'eu-west-1',
-    'eu-north-1'
-  ]
+class ReplicateTestStack extends cdk.Stack {
+
+  constructor(scope: cdk.App, id: string, props: cdk.StackProps) {
+    super(scope, id, props);
+    const key = new Key(this, 'ServiceKey', {
+      pendingWindow: Duration.days(7)
+
+    });
+    const logBucket = new s3.Bucket(this, 'LogBucket', {
+      accessControl: BucketAccessControl.LOG_DELIVERY_WRITE,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: BucketEncryption.S3_MANAGED,
+    });
+    logBucket.addToResourcePolicy(new PolicyStatement({
+      sid: 'AllowLogDelivery',
+      effect: Effect.ALLOW,
+      principals: [ new ServicePrincipal('logging.s3.amazonaws.com') ],
+      actions: ['s3:PutObject'],
+      resources: [ logBucket.arnForObjects('*') ]
+    }));
+
+    const replicatedBucket = new ReplicatedBucket(this, 'SourceBucket', {
+      bucketName: `locked-testbucket-${this.account}`,
+      encryption: s3.BucketEncryption.KMS,
+      encryptionKey: key,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      bucketKeyEnabled: true,
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+      replicateTo: ['eu-north-1', 'eu-west-1'],
+      serverAccessLogsBucket: logBucket,
+      serverAccessLogsPrefix: `S3AccessLogs/testbucket-${this.account}/`
+    });
+  }
 }
+
 
 const app = new cdk.App()
 
-new ArchiveStack(app, 'Archive', {
-  bucketProps: {},
-  stackName: `${bucketName}-archive`,
+new ReplicateTestStack(app, 'Stack', {
   terminationProtection: false,
+  env: {
+    region: 'eu-central-1',
+    account: '974281796532'
+  },
   tags: {
     stack: 'replication',
     delete: 'me',
   },
-  ...option
 })
--- aws/stacks/replicated-bucket.ts
+++ aws/stacks/replicated-bucket.ts
@@ -1,51 +1,45 @@
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as iam from 'aws-cdk-lib/aws-iam';
-import * as kms from 'aws-cdk-lib/aws-kms';
+import {IKey} from 'aws-cdk-lib/aws-kms';
 import * as cdk from 'aws-cdk-lib';
-
 import * as fs from 'fs';
 import * as path from 'path';
-//TODO: extends s3.BucketProps only have additional replicateTo
-export interface ArchiveProps extends cdk.StackProps {
+import {Construct} from 'constructs';
+
+export interface ArchiveProps extends s3.BucketProps {
   bucketName: string;
-  replications: string[];
-  bucketProps: s3.BucketProps;
+  encryptionKey: IKey;
+  replicateTo: string[];
+
 }
 
-const templateReplicationFile = path.resolve(__dirname, '../templates/replication.yml');
+const templateReplicationFile = path.resolve(__dirname, './replication.yml');
 const templateReplicationData = fs.readFileSync(templateReplicationFile).toString();
 
-//TODO: extend Construct
-export class ArchiveStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props: ArchiveProps) {
-    super(scope, id, props);
+export class ReplicatedBucket extends Construct {
+  readonly account: string;
+  readonly region: string;
+  public sourceBucket: s3.Bucket;
+  constructor(scope: Construct, id: string, props: ArchiveProps) {
+    super(scope, id);
 
-    const {replications, ...cdkBucketProps} = props;
+    const {replicateTo, ...cdkBucketProps} = props;
+    this.account = cdk.Stack.of(this).account;
+    this.region = cdk.Stack.of(this).region;
 
-//TODO: use the provided key from cdkBucketProps
-    const key = new kms.Key(this, 'Key');
-    const alias = key.addAlias(`${props.bucketName}`);
-
-    const bucket = new s3.Bucket(this, 'Bucket', {
-      bucketName: props.bucketName,
-      encryption: s3.BucketEncryption.KMS,
-      encryptionKey: alias,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      bucketKeyEnabled: true,
+    this.sourceBucket = new s3.Bucket(this, 'Bucket', {
+      ...cdkBucketProps,
       versioned: true,
-      removalPolicy: cdk.RemovalPolicy.RETAIN,
-      objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
     });
-
     //TODO: Remove for production
-    key.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
-    bucket.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+    props.encryptionKey.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+    this.sourceBucket.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
 
-    bucket.addToResourcePolicy(
+    this.sourceBucket.addToResourcePolicy(
       new iam.PolicyStatement({
         effect: iam.Effect.DENY,
         resources: [
-          bucket.bucketArn
+          this.sourceBucket.bucketArn
         ],
         actions: [
           's3:DeleteBucket'
@@ -56,11 +50,11 @@ export class ArchiveStack extends cdk.Stack {
       })
     )
 
-    bucket.addToResourcePolicy(
+    this.sourceBucket.addToResourcePolicy(
       new iam.PolicyStatement({
         effect: iam.Effect.DENY,
         resources: [
-          bucket.arnForObjects('*')
+          this.sourceBucket.arnForObjects('*')
         ],
         actions: [
           's3:DeleteObjectVersion'
@@ -154,7 +148,7 @@ export class ArchiveStack extends cdk.Stack {
       ],
       stackInstancesGroup: [
         {
-          regions: props.replications,
+          regions: props.replicateTo,
           deploymentTargets: {
             accounts: [this.account],
           },
@@ -164,10 +158,12 @@ export class ArchiveStack extends cdk.Stack {
     });
 
     stackSet.addDependsOn(stackExecutionRole.node.defaultChild as iam.CfnRole);
+
+    props.encryptionKey.grantDecrypt(replicationRole);
     replicationRole.addToPolicy(
       new iam.PolicyStatement({
         resources: [
-          bucket.bucketArn
+          this.sourceBucket.bucketArn
         ],
         actions: [
           's3:GetReplicationConfiguration',
@@ -178,7 +174,7 @@ export class ArchiveStack extends cdk.Stack {
     replicationRole.addToPolicy(
       new iam.PolicyStatement({
         resources: [
-          bucket.arnForObjects('*')
+          this.sourceBucket.arnForObjects('*')
         ],
         actions: [
           's3:GetObjectVersion',
@@ -189,22 +185,9 @@ export class ArchiveStack extends cdk.Stack {
       })
     );
 
-    //TODO: check if this works (it is shorter)
-    key.grantDecrypt(replicationRole);
     replicationRole.addToPolicy(
       new iam.PolicyStatement({
-        resources: [
-          key.keyArn
-        ],
-        actions: [
-          'kms:Decrypt'
-        ]
-      })
-    );
-
-    replicationRole.addToPolicy(
-      new iam.PolicyStatement({
-        resources: props.replications.map(
+        resources: props.replicateTo.map(
           region => `arn:aws:kms:${region}:${this.account}:alias/${props.bucketName}/replication`
         ),
         actions: [
@@ -215,7 +198,7 @@ export class ArchiveStack extends cdk.Stack {
 
     replicationRole.addToPolicy(
       new iam.PolicyStatement({
-        resources: props.replications.map(
+        resources: props.replicateTo.map(
           region => `arn:aws:s3:::${props.bucketName}-replication-${region}/*`
         ),
         actions: [
@@ -228,7 +211,7 @@ export class ArchiveStack extends cdk.Stack {
 
     replicationRole.addToPolicy(
       new iam.PolicyStatement({
-        resources: props.replications.map(
+        resources: props.replicateTo.map(
           region => `arn:aws:s3:::${props.bucketName}-replication-${region}`
         ),
         actions: [
@@ -239,12 +222,12 @@ export class ArchiveStack extends cdk.Stack {
       })
     );
 
-    const cfnBucket = bucket.node.defaultChild as s3.CfnBucket;
+    const cfnBucket = this.sourceBucket.node.defaultChild as s3.CfnBucket;
 
     // Change its properties
     cfnBucket.replicationConfiguration = {
       role: replicationRole.roleArn,
-      rules: props.replications.map(
+      rules: props.replicateTo.map(
         (region, index) => (
           {
             id: region,
@@ -271,6 +254,7 @@ export class ArchiveStack extends cdk.Stack {
         )
       )
     };
+
     cfnBucket.addDependsOn(stackSet);
 
   }
